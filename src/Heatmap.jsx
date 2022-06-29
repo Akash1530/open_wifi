0db49ac69c0d2a7e86ddbdb6deac43d41e6d9624
akash <akashkumarrana1530@gmail.com>
2022-06-29 07:28:50 +0530

    uploaded on database

--- src/Heatmap.jsx
+++ src/Heatmap.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React,{useEffect,useState} from 'react';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import { Link } from "react-router-dom";
 import Progress from "./Progress";
+import {ref,onValue} from "firebase/database"
+import {storage,database} from "./firebase";
+
 const Heatmap=()=>{
+
+   const [url,setUrl]=useState(); 
+useEffect(()=>{
+    const usersRef=ref(database,'/users');
+    onValue(usersRef,(snapshot)=>{
+        const data=snapshot.val();
+        setUrl(data.imageUrls);
+       
+       
+    });
+
+},[]);
+
     return(
         <>
            <Progress done="40"/>
+           <img src={url} style={{width:"60vw"}}/>
+           <br/>
           <Link to="/upload/heatmap/select_support_plan"><button type="button" class="btn btn-outline-success" >continue</button></Link>
         </>
     )
